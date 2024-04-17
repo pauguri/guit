4d1b73b1e5553b60ce329626c65caf78f62d9dba
Pau <pau.guri@mortensen.cat>
2024-04-17 10:03:53 +0200

    cleanup main py file

--- guit.py
+++ guit.py
@@ -3,7 +3,7 @@
 import subprocess
 import argparse
 
-# Mapping between Git commands in Catalan and their English equivalents
+# Llistat de traduccions (TODO: externalitzar a un csv)
 command_mapping = {
     "afegeix": "add",
     "arxiva": "archive",
@@ -34,20 +34,14 @@ command_mapping = {
     "remot": "remote",
 }
 
-# Main function to parse arguments and execute commands
 def main():
     parser = argparse.ArgumentParser(description="Git en català")
     parser.add_argument("command", choices=command_mapping.keys(), help="Escull una comanda de Git en català")
     parser.add_argument("args", nargs=argparse.REMAINDER, help="Arguments addicionals de Git")
     args = parser.parse_args()
 
-    # Translate the Catalan command to its English equivalent
     git_command = command_mapping[args.command]
-
-    # Construct the Git command with any additional arguments
     git_command_with_args = ["git", git_command] + args.args
-    
-    # Call the corresponding Git command
     subprocess.run(git_command_with_args)
 
 if __name__ == "__main__":
